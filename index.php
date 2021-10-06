1fa660c65d7677b76801200021aa75c433596ba0
SouradiptaPlays <92005039+SouradiptaPlays@users.noreply.github.com>
2021-10-06 08:29:29 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <link rel="stylesheet" href="css/style.css">
+        <title>DJ Dave Plays</title>
+        <meta name="viewport" content="width=device-width, initinal-scale=1.0">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    </head>
+    <body>
+        <div class="content">
+            <div class="glitch-gradient-bg"></div>
+            <div class="front-page">
+                <div class="front-page-header"><h1>DJ Dave Plays</h1></div>
+                <div class="front-page-description"><h1>Join my Discord Server for Giveaways, Game Night, Music, And More, and Check Out My YouTube Channel!</h1></div>
+                <div class="buttons">
+                    <button class="discord"><i class="fab fa-discord"></i> Discord</button>
+                    <button class="youtube"><i class="fab fa-youtube"></i> Youtube</button>
+                </div>
+            </div>
+        </div>
+        <script src="js/main.js"></script>
+    </body>
+</html>
